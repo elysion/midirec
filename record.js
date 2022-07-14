5a89cab0ac5a076e72dc0899990570e2cbe25797
Miko Kiiski <miko.kiiski@reaktor.com>
2022-07-14 22:31:01 +0300

    Implement CSV recording

--- record.js
+++ record.js
@@ -1,5 +1,4 @@
 'use strict'; // eslint-disable-line semi
-require('babel/register')
 const Rx = require('rx')
 const midi = require('midi')
 const R = require('ramda')
@@ -11,29 +10,11 @@ const Console = console
 const ClockTick = [248]
 const ClockStart = [250]
 
-const getPortNumber = name => {
-  const input = new midi.input()
-  const listMidiInputPorts = () => {
-    var inputs = []
-    for (var i = 0; i < input.getPortCount(); i++) {
-      inputs.push(input.getPortName(i))
-    }
-
-    input.closePort()
-    return inputs
-  }
-
-  var midiInputPorts = listMidiInputPorts()
-  Console.log(midiInputPorts, name, midiInputPorts.indexOf(name))
-  if (midiInputPorts.indexOf(name) == -1) {
-    throw `Port ${name} not found! Available ports: ${midiInputPorts.join(', ')}`
-  }
-  return midiInputPorts.indexOf(name)
-}
+const valToSignalKey = val => `${val[0]}.${val[1]}`
 
 const main = () => {
+  Console.log(process.argv)
   const filename = process.argv[3]
-  const messageDeviceName = process.argv[2]
   const scriptName = path.basename(__filename)
 
   Console.log('Starting recording to', filename)
@@ -42,28 +23,32 @@ const main = () => {
   stdin.finally(() => fileStream.end())
 
   const clockInput = new midi.input()
-  // input.openVirtualPort('Test Input')
-  clockInput.openVirtualPort(scriptName)
+  clockInput.openVirtualPort(`${scriptName} clock`)
   clockInput.ignoreTypes(false, false, false)
-  var clockInputSubject = new Rx.Subject()
-  clockInput.on('message', (deltaTime, message) => {clockInputSubject.onNext([deltaTime, message])})
+  const clockInputSubject = new Rx.Subject()
+  clockInput.on('message', (deltaTime, message) => {
+    clockInputSubject.onNext([deltaTime, message])
+  })
   const clock = clockInputSubject.map(R.nth(1)).filter(R.flip(R.contains)([ClockTick, ClockStart]))
 
   const messageInput = new midi.input()
-  // input.openVirtualPort('Test Input')
-  messageInput.openPort(getPortNumber(messageDeviceName))
-  var messageInputSubject = new Rx.Subject()
-  messageInput.on('message', (deltaTime, message) => {messageInputSubject.onNext([deltaTime, message])})
+  messageInput.openVirtualPort(`${scriptName} output`)
+  const messageInputSubject = new Rx.Subject()
+  messageInput.on('message', (deltaTime, message) => {
+    messageInputSubject.onNext([deltaTime, message])
+  })
   const messages = messageInputSubject.map(R.nth(1)).filter(R.complement(R.flip(R.contains)([ClockTick, ClockStart])))
 
   const initializedMessageOutput = new midi.output()
   initializedMessageOutput.openVirtualPort(`${scriptName} initialized`)
 
-  const position = clock.scan(([phrase, bar, beat, tick], message) => {
+  const position = clock.scan(([phrase, bar, beat, tick, raw], message) => {
     if (R.equals(message, ClockStart)) {
-      return [0,0,0,0]
+      return [0, 0, 0, 0, 0]
     }
 
+    // TODO: should not mutate here...
+    raw = raw + 1
     const tickOverflow = tick === 23 ? 1 : 0
     tick = (tick + 1) % 24
     const beatOverflow = beat === 3 && tickOverflow ? 1 : 0
@@ -71,18 +56,46 @@ const main = () => {
     const barOverflow = bar === 3 && beatOverflow ? 1 : 0
     bar = (bar + beatOverflow) % 4
     phrase += barOverflow
-    return [phrase, bar, beat, tick]
-  }, [0, 0, 0, 0])
+    return [phrase, bar, beat, tick, raw]
+  }, [0, 0, 0, 0, 0])
 
-  messages.withLatestFrom(position, Array)
+  const currentStateStream = messages.withLatestFrom(position, Array)
     .map(R.reverse)
     .tap(message => Console.log(message))
-    .map(JSON.stringify)
+    .scan((currentState, [time, message]) => {
+      currentState.time = time
+      const signalKey = valToSignalKey(message)
+      let column = currentState.messages.findIndex(({ key }) => key === signalKey)
+      if (column === -1) {
+        column = currentState.messages.length
+      }
+      currentState.messages[column] = { key: signalKey, value: message[2] }
+      return currentState
+    }, { messages: [] })
+
+  currentStateStream
+    .map(({ time, messages }) => {
+      const messagesString = messages.map(({ value }) => value).join(',')
+      return `${time[4]},${time.slice(0, 4).join('.')},${messagesString}`
+    })
+    .tap(message => Console.log(message))
     .subscribe(serializedData => fileStream.write(serializedData + '\n'))
 
-  initializedMessageOutput.sendMessage([ 180, 48, 125 ])
+  initializedMessageOutput.sendMessage([180, 48, 125])
+
+  const exitStream = Rx.Observable.fromEvent(process, 'exit')
+  exitStream.subscribe(() => Console.log('exit'))
+
+  currentStateStream.sample(exitStream)
+    .map(({ messages }) => `tick,position,${messages.map(({ key }) => key).join(',')}`)
+    .map(serializedData => fileStream.write(serializedData + '\n')) // TODO: replace map with a better function
+    .subscribe(() => closePorts())
+
   RxNode.fromStream(process.stdin, 'end')
-    .subscribe((input) => {Console.log('input', input); initializedMessageOutput.sendMessage([ 180, 48, 125 ])})
+    .subscribe((input) => {
+      Console.log('input', input);
+      initializedMessageOutput.sendMessage([180, 48, 125])
+    })
 
   const closePorts = () => {
     Console.log('Closing ports')
@@ -91,7 +104,6 @@ const main = () => {
     initializedMessageOutput.closePort()
   }
 
-  process.on('exit', closePorts)
   process.on('SIGINT', closePorts)
   process.on('uncaughtException', closePorts)
 }
